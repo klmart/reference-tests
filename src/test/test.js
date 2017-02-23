0008075384181dca3496efeaed82315a3500fd02
Marat Kulbaev <mkulbaev@spindletop.software>
2017-02-23 12:32:55 +0600

    Done

--- src/test/test.js
+++ src/test/test.js
@@ -6,13 +6,24 @@ describe('payload', function () {
 
   // in this tests try to use as least as possible number of assignments
 
+  const CARS = payload.data.filter(function (item) {
+    return item.type === 'Car'
+  });
+
   it('car quantity with owners older than 20 years', function () {
 
     let answer;
+    let carOwners = [];
 
-    /**
-     * you code here
-     */
+    CARS.forEach(function (car) {
+      car.owners.forEach(function (owner) {
+        carOwners.push(owner);
+      });
+    });
+
+    answer = carOwners.filter(function (owner) {
+      return owner.personalInfo.age > 20
+    }).length;
 
     assert.equal(answer, 2);
 
@@ -22,9 +33,16 @@ describe('payload', function () {
 
     let answer;
 
-    /**
-     * you code here
-     */
+    let carColors = CARS.map(function (car) {
+      return car.attrs.color
+    });
+
+    function uniqueVal(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+
+    answer = carColors.filter(uniqueVal)
+                      .join(',');
 
     assert.equal(answer, 'red,yellow');
 
@@ -34,9 +52,20 @@ describe('payload', function () {
 
     let answer;
 
-    /**
-     * you code here
-     */
+    let vehicles = payload.data.filter(function (item) {
+      if (item.type === 'Car') {
+        return item
+      }
+
+      if (item.type === 'Bicycle') {
+        return item
+      }
+    });
+
+    answer = vehicles.map(function (vehicle) {
+      return vehicle.id
+    })
+                     .join(',');
 
     assert.equal(answer, '1,3,6,4,2');
 
@@ -45,10 +74,11 @@ describe('payload', function () {
   it('summary price of all items', function () {
 
     let answer;
+    let priceOfItems = payload.data.map(function (item) {
+      return item.attrs.price
+    });
 
-    /**
-     * you code here
-     */
+    answer = priceOfItems.reduce((a, b) => a + b, 0);
 
     assert.equal(answer, 42800);
 
@@ -58,9 +88,20 @@ describe('payload', function () {
 
     let answer;
 
-    /**
-     * you code here
-     */
+    let johnsThings = [];
+
+    payload.data.forEach(function (item) {
+      item.owners.forEach(function (owner) {
+        if (owner == people.johnSmith) {
+          johnsThings.push(item);
+        }
+      });
+    });
+
+    answer = johnsThings.map(function (thing) {
+      return thing.attrs.price
+    })
+                        .reduce((a, b) => a + b, 0);
 
     assert.equal(answer, 25000);
 
@@ -69,10 +110,28 @@ describe('payload', function () {
   it('all cities', function () {
 
     let answer;
+    let owners = [];
+    let cities = [];
 
-    /**
-     * you code here
-     */
+    payload.data.forEach(function (item) {
+      item.owners.forEach(function (owner) {
+        owners.push(owner);
+      })
+    });
+
+    function uniqueVal(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+
+    let uniqueOwners = owners.filter(uniqueVal);
+
+    uniqueOwners.forEach(function (owner) {
+      owner.cities.forEach(function (city) {
+        cities.push(city)
+      })
+    });
+
+    answer = cities.join(',');
 
     assert.equal(answer, 'New York,Boston,Columbia,Rapture');
 
